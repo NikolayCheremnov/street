4446b1a3a2df8bf3b3d6c09b1d7587cf14da0f09
coolnick1999 <coolnick1999@gmail.com>
2021-01-09 03:41:34 +0700

    Axes, floor and flashlights added

--- main.js
+++ main.js
@@ -13,16 +13,20 @@ function loadObj(mtlURL, objURL, setPosition) {
 }
 
 function main() {
+    // main color
+    var main_color = 0xFFFFFF
+
 	// Create scene, camera and render
 	var scene = new THREE.Scene();
-	scene.background = new THREE.Color(0x485E5C);
+	scene.background = new THREE.Color(main_color);
 
 	const FOV = 90;
 	const ASPECT = window.innerWidth/window.innerHeight;
 	const NEAR = 0.1;
 	const FAR = 100;
 	
-	var camera = new THREE.PerspectiveCamera(FOV, ASPECT, NEAR, FAR);
+    var camera = new THREE.PerspectiveCamera(FOV, ASPECT, NEAR, FAR);
+    camera.position.x = 5;
     camera.position.z = -5;
     camera.position.y = 5;
 
@@ -34,20 +38,43 @@ function main() {
 	// Camera controls
 	const controls = new THREE.OrbitControls(camera, renderer.domElement);
 	controls.update();
-	
+    
+    // axes
+    function drawStretch(x1=0, y1=0, z1=0, x2=1, y2=1, z2=1, color=0x000000) {
+        var geometry = new THREE.Geometry();
+        geometry.vertices.push(new THREE.Vector3(x1,y1,z1));
+        geometry.vertices.push(new THREE.Vector3(x2,y2,z2));
+        var material = new THREE.LineBasicMaterial({color: color});
+        var line = new THREE.Line(geometry, material);
+        scene.add(line)
+    }
+    // axes drawing. x : red, y: green, z: blue
+    drawStretch(0, 0, 0, 100, 0, 0, 0xFF0000);
+    drawStretch(0, 0, 0, 0, 100, 0, 0x00FF00);
+    drawStretch(0, 0, 0, 0, 0, 100, 0x0000FF);
+
+    // spehere adding
+    function drawSphhere() {
+        var geometry = new THREE.SphereGeometry(1, 32, 32);
+        var material = new THREE.MeshBasicMaterial({color:0x00EF00});
+        var sphere = new THREE.Mesh(geometry, material);
+        sphere.position.y = 5;
+        scene.add(sphere);
+    }
+
 	// Floor
-	const planeSize = 10;
+	const planeSize = 20;
 	const textureLoader = new THREE.TextureLoader();
 	
-	const floorTexture = textureLoader.load(URL + "models/textures/kover.jpg");
+	const floorTexture = textureLoader.load(URL + "/textures/asphalt.jpg");
 	floorTexture.wrapS = THREE.RepeatWrapping;
 	floorTexture.wrapT = THREE.RepeatWrapping;
 	floorTexture.magFilter = THREE.NearestFilter;
 	
-	const repeats = 6;
+	const repeats = 3;
 	floorTexture.repeat.set(repeats, repeats);
 
-	const planeGeo = new THREE.PlaneBufferGeometry(planeSize, planeSize);
+	const planeGeo = new THREE.PlaneBufferGeometry(planeSize*3, planeSize);
 	const planeMat = new THREE.MeshPhongMaterial({
 		map: floorTexture,
 		side: THREE.DoubleSide,
@@ -55,104 +82,55 @@ function main() {
 	
 	const floor = new THREE.Mesh(planeGeo, planeMat);
 	floor.rotation.x = Math.PI * -0.5;
-	floor.position.set(0, 0.1, 0);
+	floor.position.set(0, 0, 0);
 	floor.receiveShadow = true;
 	
 	scene.add(floor);
 
-	// // Adding room walls
-	// let width = 10;
-	// let height = 5;
-	// let depth = 10;
-	
-	// var geometry = new THREE.BoxGeometry(width, height, depth);
-	// var material = new THREE.MeshPhongMaterial({
-	// 	color: 0xffffff,
-	// 	side: THREE.DoubleSide
-	// });
+    // adding ambient light
+    const ambient_light = new THREE.AmbientLight(main_color, 1)
+    scene.add(ambient_light)
 
-	// var roomCube = new THREE.Mesh(geometry, material);
-	// roomCube.receiveShadow = true;
-	// roomCube.position.set(0, 2.5, 0);
-	
-    // scene.add(roomCube);
-    
-    // drow bowl
-    function drowBowl(x=0, y=0, z=0, colorBowl=0xff0000, colorFood=0x057D9F) {
-        // top part
-        var radius = 3;
-        var height = 3;
-        
-        var fiberGeometry = new THREE.CylinderGeometry(radius,
-            radius, height);
-        var fiberMaterial = new THREE.MeshPhongMaterial({
-            color: colorBowl,
-        });
-        var food = new THREE.Mesh(fiberGeometry, fiberMaterial);
-        food.position.set(1 + x, 0.2 + y, -4 + z);
-        food.scale.set(0.1, 0.1, 0.1);
-        
-        scene.add(food);
-        // bottom part
-        var radius = 4;
-        var height = 2;
-        
-        var fiberGeometry = new THREE.CylinderGeometry(radius,
-            radius, height);
-        var fiberMaterial = new THREE.MeshPhongMaterial({
-            color: colorBowl,
-        });
-        var food = new THREE.Mesh(fiberGeometry, fiberMaterial);
-        food.position.set(1 + x, 0.1 + y, -4 + z);
-        food.scale.set(0.1, 0.1, 0.1);
-        
-        scene.add(food);
-
-        // dog food
-        var radius = 2;
-        var height = 3;
-        
-        var fiberGeometry = new THREE.CylinderGeometry(radius,
-            radius, height);
-        var fiberMaterial = new THREE.MeshPhongMaterial({
-            color: colorFood,
-        });
-        var food = new THREE.Mesh(fiberGeometry, fiberMaterial);
-        food.position.set(1 + x, 0.21 + y, -4 + z);
-        food.scale.set(0.1, 0.1, 0.1);
-        scene.add(food);
-    }
-
-    drowBowl();
-    drowBowl(-1, 0, 0, 0xBC008D, 0x431B19)
-    
-    // drow lamp
-    function drowLamp(x=0, y=0, z=0, colorSteec=0xff0000, colorLamp=0xffff00) {
-        // top part
-        var radius = 0.05;
-        var height = 3;
+    // draw flashkights
+    function drawLamp(x=0, y=0, z=0, steelColor=0x574C4C, lampColor=0xffff00) {
+        // stick
+        var radius = 0.1;
+        var stickHeight = 8;
         
         var fiberGeometry1 = new THREE.CylinderGeometry(radius,
-            radius, height);
+            radius, stickHeight);
         var fiberMaterial1 = new THREE.MeshPhongMaterial({
-            color: colorSteec,
+            color: steelColor,
         });
         var p1 = new THREE.Mesh(fiberGeometry1, fiberMaterial1);
-        p1.position.set(x, 1.5 + y, z);
+        p1.position.set(x, y + stickHeight / 2, z);
         
         scene.add(p1);
 
-        var radius = 0.5;
-        var height = 0.5;
-        // scene.add(p2)
-        var geometry = new THREE.ConeBufferGeometry( radius, height );
+        var radiusTop = 0.25;
+        var radiusBottom = 0.5;
+        var height = 1;
+        
+        // steel cone
+        var geometry = new THREE.CylinderGeometry( radiusTop, radiusBottom, height);
         var material = new THREE.MeshBasicMaterial({
-            color: colorLamp
+            color: steelColor,
         });
         var cone = new THREE.Mesh( geometry, material);
-        cone.position.set(x, 3 + y, z);
-        scene.add(cone);
-        
+        cone.position.set(x, y + stickHeight - 0.3, z-0.3);
+        cone.rotation.x = Math.PI / 4;
+        scene.add(cone); 
+
+        // lamp cone
+        var geometry = new THREE.CylinderGeometry( radiusTop, radiusBottom, height);
+        var material = new THREE.MeshBasicMaterial({
+            color: lampColor,
+        });
+        var cone = new THREE.Mesh( geometry, material);
+        cone.position.set(x, y + stickHeight-0.4, z-0.4);
+        cone.rotation.x = Math.PI / 4;
+        scene.add(cone); 
+
         // bottom part
         var radius = 0.4;
         var height = 0.2;
@@ -160,100 +138,26 @@ function main() {
         var fiberGeometry2 = new THREE.CylinderGeometry(radius,
             radius, height);
         var fiberMaterial2 = new THREE.MeshPhongMaterial({
-            color: colorSteec,
+            color: steelColor,
         });
         var p2 = new THREE.Mesh(fiberGeometry2, fiberMaterial2);
         p2.position.set(x, 0.1 + y,z);
         
         scene.add(p2);
 
-        return p1;
+        // light adding
+        var lampLightIntensity = 2;
+        var lampLightDistance = 15;
+        const lampLight = new THREE.PointLight( 0xffffff, lampLightIntensity, lampLightDistance);
+        lampLight.position.set(x, y+stickHeight, z-6);
+        lampLight.castShadow = true;
+        scene.add(lampLight)
     }
-    drowLamp(-0.5, 0, 3)
+    //drawLamp(0, 0, 0)
+    for (var i = -30; i < 31; i += 15)
+        drawLamp(i, 0, 9)
 
-	// Lamp light
-	var lampLightIntensity = 1;
-	var lampLightDistance = 10;
-
-	const lampLight1 = new THREE.PointLight( 0xffffff, lampLightIntensity,
-		 lampLightDistance);
-	lampLight1.position.set(0, 4.5, -5);
-	lampLight1.castShadow = true;
-	
-    scene.add(lampLight1);
-    
-    // "window" light
-	var lampLightIntensity = 1;
-	var lampLightDistance = 5;
-
-	const lampLight2 = new THREE.PointLight( 0xFFED90, lampLightIntensity,
-		 lampLightDistance);
-    lampLight2.position.set(-0.5, 2.5, 3);
-    lampLight2.castShadow = true;
-
-    scene.add(lampLight2);
-
-    scaleSet = function(obj, k) {
-        obj.scale.set(k, k, k)
-    }
-
-	// dining room
-	loadObj(URL + "models/dr.mtl", URL + "models/dr.obj",
-		(dr) => {
-			// First chair
-			dr.traverse(function(child) {
-				child.castShadow = true;
-				child.receiveShadow = true;
-			});
-
-            scaleSet(dr, 60);
-            dr.rotation.y = Math.PI * -0.3;
-			
-			dr.position.set(-0.2, 2.35, 2.5);
-
-			scene.add(dr);
-		 }
-    );
-    // poly_chair
-    loadObj(URL + "models/poly_chair.mtl", URL + "models/poly_chair.obj",
-    (dog) => {
-        //
-        dog.traverse(function(child) {
-            child.castShadow = true;
-            child.receiveShadow = true;
-        });
-        scaleSet(dog, 0.7)
-        dog.position.set(-1, 0.1, -1.5);
-
-        scene.add(dog);
-    });
-
-    // Mesh_Beagle
-    loadObj(URL + "models/Mesh_Beagle.mtl", URL + "models/Mesh_Beagle.obj",
-    (dog) => {
-        //
-        dog.traverse(function(child) {
-            child.castShadow = true;
-            child.receiveShadow = true;
-        });
-        dog.position.set(0, 0.1, 0.5);
-
-        scene.add(dog);
-    });
-
-    // Mesh_Poodle
-    loadObj(URL + "models/Mesh_Poodle.mtl", URL + "models/Mesh_Poodle.obj",
-    (dog) => {
-        //
-        dog.traverse(function(child) {
-            child.castShadow = true;
-            child.receiveShadow = true;
-        });
-        scaleSet(dog, 1.2)
-        dog.position.set(-1, 0.1, 0);
-        scene.add(dog);
-    });
-
+    // animate
 	var animate = function() {
 		requestAnimationFrame(animate);
 
